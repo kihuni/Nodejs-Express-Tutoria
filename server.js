8c16a6724e5b9c0b38e61ad5aa5e79cf5f8c54ed
kihuni <stephenkihuni55@gmail.com>
2022-04-24 15:24:22 +0300

    added final code

--- server.js
+++ server.js
@@ -1,120 +1,81 @@
-// Using Node 
-// const http = require('http')
-
-// let notes = [
-//     {
-//       id: 1,
-//       content: "HTML is easy",
-//       date: "2022-05-30T17:30:31.098Z",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only Javascript",
-//       date: "2022-05-30T18:39:34.091Z",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       date: "2022-05-30T19:20:14.298Z",
-//       important: true
-//     }
-//   ]
-
-// const app = http.createServer((req, res) =>{
-//     res.writeHead(200,{'Content-Type': 'application/json'})
-//     res.end(JSON.stringify(notes))
-// })
-
-// const PORT = 3001
-
-// app.listen(PORT)
-
-// console.log(`Server is listening on ${PORT}`)
-
-
-//using Express
-
 const express = require('express')
-
 const app = express()
 
-//body parser
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-01-10T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-01-10T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-01-10T19:20:14.298Z",
+    important: true
+  }
+]
+
 app.use(express.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-app.get('/', (req,res)=>{
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-//route that handles get request
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
-app.get('/api/notes/:id', (req, res)=>{
-
-//  // testing every step of the route process using console.log
-//   const id =  Number(req.params.id)
-//   console.log(id)
-
-//   const note = notes.find(note => {
-//     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-
-//     return note.id === id
-//   })
-  
-//  console.log(note)
-//   //matched request returned to the sender
-//   res.json(note)
-
-const id = Number(req.params.id)
-
-const note = notes.find(note => note.id === id)
-
-if(note){
-  res.json(note)
-}else{
-  res.status(404).end()
-}
-
+  response.status(204).end()
 })
 
-// route for handling delete request
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-app.delete('/api/notes/:id', (req, res) =>{
-  const id = Number(req.params.id)
-
- notes = notes.filter(note => note.id !== id)
-
-  res.status(204).end()
-})
-
-
-//route for handling post request
-app.post('/api/notes', (req, res) =>{
-  const note = req.body
-  console.log(note)
-
-  res.json(note)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
